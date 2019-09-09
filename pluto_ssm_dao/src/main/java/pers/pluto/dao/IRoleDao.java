393edef9478a99d02e8d5d197e181e9c092beea3
Capriciousness <qq513819387@outlook.com>
2019-09-09 21:10:46 +0800

    解决订单分页查询问题，并且可以选择每页显示条数

---
+++ pluto_ssm_dao/src/main/java/pers/pluto/dao/IRoleDao.java
@@ -0,0 +1,13 @@
+package pers.pluto.dao;
+
+import org.apache.ibatis.annotations.Select;
+import pers.pluto.domain.Role;
+
+import java.util.List;
+
+public interface IRoleDao {
+
+    //根据用户id查询出所有对应的角色
+    @Select("select * from role where id in (select roleId from users_role where userId=#{userId})")
+    public List<Role> findRoleByUserId(String userId) throws Exception;
+}
